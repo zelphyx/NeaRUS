2f85ec93ceff979061d829a90b9275ba00cf6be2
zelphyx <rizqy.astiko19@gmail.com>
2024-04-22 06:36:10 +0700

    feat [IR} Fitur Awal Bot Auto Response

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::match(['get','post'], '/botman',[\App\Http\Controllers\BotController::class,'handle']);
