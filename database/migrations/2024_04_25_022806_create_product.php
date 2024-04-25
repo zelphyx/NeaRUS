74bbd0123a17e18cb0c2e0d21810616bcb7ee691
zelphyx <rizqy.astiko19@gmail.com>
2024-04-25 09:54:50 +0700

    feat [IR] Product API

---
+++ database/migrations/2024_04_25_022806_create_product.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('productname');
+            $table->string('location');
+            $table->string('category');
+            $table->string('fasilitas');
+            $table->foreignId('roomid');
+            $table->string("about");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
