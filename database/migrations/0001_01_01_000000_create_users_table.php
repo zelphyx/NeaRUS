cdd066b07c71bf3991d1da25020562b373bdd872
JustNandoo <nandofajar1211@gmail.com>
2024-06-02 23:04:17 +0700

    feat [AF] UI ResetPass & VerifikasiBerhasil

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -33,7 +33,7 @@ return new class extends Migration
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primar y();
+            $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
