1532a58a44dc9cedf469b3e43abcaa3f11e1bc6b
zelphyx <rizqy.astiko19@gmail.com>
2024-06-06 03:19:13 +0700

    fixed [IR] Fixing Edit Issue

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ Route::get('/user',[\App\Http\Controllers\API\User\AuthUserController::class,'in
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::controller(\App\Http\Controllers\API\User\ProfileController::class)->group(function () {
-        Route::post('profile/update', 'updateProfile');
+        Route::put('profile/update', 'updateProfile');
         Route::post('profile/reset-password','profileresetpass');
         Route::post('profile/add-personal-data','addPersonalData');
     });
