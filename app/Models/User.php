3c2e3d0f82b50fc9806cc9df50c84a63b4010059
zelphyx <rizqy.astiko19@gmail.com>
2024-06-06 03:15:49 +0700

    fixed [IR] Fixing Photo Profile Issue

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,4 +76,8 @@ class User extends Authenticatable implements MustVerifyEmail
             'password' => 'hashed',
         ];
     }
+    public function getKeyName()
+    {
+        return 'ownerId';
+    }
 }
