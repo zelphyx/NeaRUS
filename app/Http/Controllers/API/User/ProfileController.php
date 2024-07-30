d860b24cdba12e3688583f6453579f2fb49f998a
zelphyx <rizqy.astiko19@gmail.com>
2024-07-30 08:56:30 +0700

    feat [IR] Balance Management

--- app/Http/Controllers/API/User/ProfileController.php
+++ app/Http/Controllers/API/User/ProfileController.php
@@ -158,4 +158,14 @@ class ProfileController extends Controller
 
         return response()->json(['message' => 'Password has been updated successfully']);
     }
+
+    public function currentprofile(Request $request) {
+        $user = Auth::user();
+
+        if (!$user) {
+            return response()->json(['message' => 'User not authenticated'], 401);
+        }
+
+        return response()->json(['user' => $user], 200);
+    }
 }
