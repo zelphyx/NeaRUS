3de2e2a953712a8e010a1f949a19125807ef3063
JustNandoo <nandofajar1211@gmail.com>
2024-08-26 23:56:15 +0700

    fix [AF] DesignResetPw

--- resources/views/resetpass.blade.php
+++ resources/views/resetpass.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Reset</title>
-    @vite('resources/css/app.css')
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
