020e0744efbb85d1f6fdc42a3c52f404123b58f3
JustNandoo <nandofajar1211@gmail.com>
2024-08-26 23:46:57 +0700

    fix [AF] DesignResetPw

--- resources/views/resetpass.blade.php
+++ resources/views/resetpass.blade.php
@@ -8,6 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- Font Awesome for Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="./output.css" rel="stylesheet">
 
     <style>
         /* External or global CSS for background */
