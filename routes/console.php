78dff702ac982bad77395d2144419674e1f3b2b1
zelphyx <rizqy.astiko19@gmail.com>
2024-09-02 10:16:09 +0700

    fix [IR] FixProduct

--- routes/console.php
+++ routes/console.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
-(new Illuminate\Console\Scheduling\Schedule)->command('orders:clean-expired')->daily();
+(new Illuminate\Console\Scheduling\Schedule)->command('orders:clean-expired');
 
