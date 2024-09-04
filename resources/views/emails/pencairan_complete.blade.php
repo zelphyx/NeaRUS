316fd6b3040e8d480566c200afb697f408da53a5
zelphyx <rizqy.astiko19@gmail.com>
2024-09-04 09:54:22 +0700

    feat [IR] add total kamar yg disewakan

--- resources/views/emails/pencairan_complete.blade.php
+++ resources/views/emails/pencairan_complete.blade.php
@@ -6,11 +6,16 @@
     <title>Permintaan Akses Diterima</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-gray-100 flex items-center justify-center min-h-screen">
-<div class="bg-white shadow-lg rounded-lg p-8 max-w-md text-center">
-    <h1 class="text-2xl font-bold text-green-500 mb-4">Selamat, Pencairan Anda Sukses!</h1>
-    <p class="text-gray-700">
-        Permintaan Akses Owner Anda telah diterima oleh admin kami. Silakan login dengan email dan password yang telah Anda daftarkan.
+<body class="bg-gradient-to-r from-green-400 to-blue-500 flex items-center justify-center min-h-screen">
+<div class="bg-white shadow-2xl rounded-xl p-10 max-w-lg text-center">
+    <div class="flex items-center justify-center mb-6">
+        <svg class="w-16 h-16 text-green-500" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m1-3a9 9 0 11-8 0 9 9 0 018 0z"></path>
+        </svg>
+    </div>
+    <h1 class="text-3xl font-extrabold text-gray-800 mb-4">Pencairan Berhasil!</h1>
+    <p class="text-gray-600 mb-6">
+        Pencairan Anda telah berhasil. Silakan cek saldo rekening atau saldo e-wallet Anda.
     </p>
 </div>
 </body>
