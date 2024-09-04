28dfd8a3f1644f9ed2e347c2bc2174c2b5d30309
zelphyx <rizqy.astiko19@gmail.com>
2024-09-04 10:04:25 +0700

    feat [IR] add total kamar yg disewakan

--- resources/views/emails/pencairan_complete.blade.php
+++ resources/views/emails/pencairan_complete.blade.php
@@ -12,8 +12,9 @@
             <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="500px" style="background-color: #ffffff; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.15); border-radius: 10px;">
                 <tr>
                     <td align="center" style="padding: 20px;">
-                        <img src="https://via.placeholder.com/64x64.png?text=✔" alt="Success Icon" width="64" height="64" style="display: block; margin-bottom: 20px;">
-                        <h1 style="font-size: 24px; font-weight: bold; color: #2d3748; margin-bottom: 20px;">Pencairan Berhasil!</h1>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" style="width: 64px; height: 64px; color: #38b2ac; margin-bottom: 20px;">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m1-3a9 9 0 11-8 0 9 9 0 018 0z" />
+                        </svg>                        <h1 style="font-size: 24px; font-weight: bold; color: #2d3748; margin-bottom: 20px;">Pencairan Berhasil!</h1>
                         <p style="font-size: 16px; color: #4a5568; margin-bottom: 30px;">
                             Pencairan Anda telah berhasil. Silakan cek saldo rekening atau saldo e-wallet Anda.
                         </p>
